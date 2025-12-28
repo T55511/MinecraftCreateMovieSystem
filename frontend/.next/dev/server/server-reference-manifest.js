dde727c8be0f4535a30a90721781c9d2d5d55564
T55511 <139596487+T55511@users.noreply.github.com>
2025-12-28 22:51:54 +0900

    2025/12/28 修正

--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"UdAf5mEmsqe9o/Mgl74GidJziGvII9IpBLCiCd2/HJk=\"\n}"
--- frontend/.next/dev/static/chunks/src_app_studio_page_tsx_15c15fcf._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_studio_page_tsx_b0837974._.js"
-],
-    source: "dynamic"
-});
